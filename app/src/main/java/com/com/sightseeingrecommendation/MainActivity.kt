8b26359af82425eb3e1b3c2ef173cbd5aac3fcf3
sangjin-Lee <sjin8855@naver.com>
2021-07-06 22:43:24 +0900

    FEAT-0001 Kakao Map API 등록 및 맵 띄우기까지 커밋

---
+++ app/src/main/java/com/com/sightseeingrecommendation/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.com.sightseeingrecommendation
+
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import net.daum.mf.map.api.MapView
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //getHashKey();
+
+        val mapView = MapView(this)
+
+        val mapViewContainer = findViewById<View>(R.id.map_view) as ViewGroup
+        mapViewContainer.addView(mapView)
+    }
+
+    // 앱 등록 해쉬값 확인용 소스
+    private fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
+    }
+
+}
